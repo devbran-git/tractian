b0f6ccea05e0034e792084ed1e7353df09ce0f23
devbran <d-evbran@outlook.com>
2022-06-14 22:51:04 -0300

    feat: basic settings added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from 'antd';
 
-function App() {
+const App: React.FC = () => {
+  const { Title } = Typography;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title style={{ marginBottom: 0 }} level={4}>
+        Tractian Challenge
+      </Title>
+    </>
   );
-}
+};
 
 export default App;
