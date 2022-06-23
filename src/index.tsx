38e7d1f2d88e383e105ec66bf8e14235281d2219
devbran <d-evbran@outlook.com>
2022-06-23 09:16:19 -0300

    fix: css link added

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 
 import { ConfigProvider } from 'antd';
 import ptBR from 'antd/lib/locale/pt_BR';
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
